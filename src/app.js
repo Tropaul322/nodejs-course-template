f83166766c0c650bbcb6908ccf46e47ff9906666
Paul <tropaul322@gmail.com>
2020-10-19 19:29:46 +0300

    feat: add logger

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/boards.router');
 const tasksRouter = require('./resources/tasks/tasks.router');
+const { requestLogger } = require('./common/logger');
+const logger = require('./common/logger');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -12,6 +14,9 @@ const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// Add express middleware which will log incoming requests to service (url, query parameters, body).
+app.use(requestLogger);
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
@@ -24,9 +29,19 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 boardRouter.use('/:boardId/tasks', tasksRouter);
-app.use((req, res, next) => {
-  res.status(404).send('Something broke!');
+
+// Add express middleware which will log all unhandled errors and return a standard message with HTTP code 500 (Internal Server Error).
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send(err.message);
+  } else {
+    logger.error(err.stack);
+    res.status(500).send('Internal Server Error');
+  }
   next();
 });
+
+// throw Error('Oops!');
+// Promise.reject(Error('Oops!'));
 
 module.exports = app;
--- src/resources/tasks/tasks.router.js
+++ src/resources/tasks/tasks.router.js
@@ -1,42 +1,63 @@
 const router = require('express').Router({ mergeParams: true });
 const taskService = require('./tasks.service');
+const { logger } = require('../../common/logger');
 
 router.route('/').get(async (req, res) => {
-  const boardId = req.params.boardId;
-  const tasks = await taskService.getAll(boardId);
-  res.status(200).json(tasks);
+  try {
+    const boardId = req.params.boardId;
+    const tasks = await taskService.getAll(boardId);
+    res.status(200).json(tasks);
+  } catch (e) {
+    logger.error('error', e.stack);
+  }
 });
 router.route('/:id').get(async (req, res) => {
-  const boardId = req.params.boardId;
-  const taskID = req.params.id;
-  const task = await taskService.getTask(boardId, taskID);
-  if (task) {
-    res.status(200).json(task);
-  } else {
-    res.status(404).send('cannot find task');
+  try {
+    const boardId = req.params.boardId;
+    const taskID = req.params.id;
+    const task = await taskService.getTask(boardId, taskID);
+    if (task) {
+      res.status(200).json(task);
+    } else {
+      res.status(404).send('cannot find task');
+    }
+  } catch (e) {
+    logger.error('error', e.stack);
   }
 });
 
 router.route('/').post(async (req, res) => {
-  const boardId = req.params.boardId;
-  const task = await taskService.postTask(boardId, req.body);
-  res.status(200).json(task);
+  try {
+    const boardId = req.params.boardId;
+    const task = await taskService.postTask(boardId, req.body);
+    res.status(200).json(task);
+  } catch (e) {
+    logger.error('error', e.stack);
+  }
 });
 router.route('/:id').put(async (req, res) => {
-  const boardId = req.params.boardId;
-  const taskID = req.params.id;
-  const body = req.body;
-  const task = await taskService.changeTask(boardId, taskID, body);
-  if (task) {
-    res.status(200).json(task);
-  } else res.status(404).json({});
+  try {
+    const boardId = req.params.boardId;
+    const taskID = req.params.id;
+    const body = req.body;
+    const task = await taskService.changeTask(boardId, taskID, body);
+    if (task) {
+      res.status(200).json(task);
+    } else res.status(404).json({});
+  } catch (e) {
+    logger.error('error', e.stack);
+  }
 });
 router.route('/:id').delete(async (req, res) => {
-  const boardId = req.params.boardId;
-  const taskID = req.params.id;
-  const deleted = await taskService.deleteTask(boardId, taskID);
-  if (deleted) {
-    res.status(204).json([]);
-  } else res.status(404).json({});
+  try {
+    const boardId = req.params.boardId;
+    const taskID = req.params.id;
+    const deleted = await taskService.deleteTask(boardId, taskID);
+    if (deleted) {
+      res.status(204).json([]);
+    } else res.status(404).json({});
+  } catch (e) {
+    logger.error('error', e.stack);
+  }
 });
 module.exports = router;
